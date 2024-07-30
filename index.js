fa697a0e709ef8723ffb8ffdef07089c00ef6c8e
Aritra Bera <aritrabera69@gmail.com>
2024-07-31 04:31:10 +0530

    Mongodb connection create

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
-
+const express = require("express");
+const bodyPaser = require("body-parser");
+const dotenv = require("dotenv");
 const colors = require("colors");
 
-const express = require("express");
-
-const bodyPaser = require("body-parser");
-
-const db = require("./config/db")
+const db = require("./config/db");
 
 const app = express();
 
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
 
 app.listen(PORT, async () => {
     console.log(`Server is Started at ${PORT}`.bgBlue);
