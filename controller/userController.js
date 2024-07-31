cd3031631cfe5a413e7773b3d1a60199a67d59c0
Aritra Bera <aritrabera69@gmail.com>
2024-07-31 06:05:04 +0530

    .....

--- controller/userController.js
+++ controller/userController.js
@@ -1,30 +1,77 @@
 const User = require("../model/userModel");
 
-const test = async(req,res)=>{
+const test = async (req, res) => {
     try {
         return res.json("Hello World!");
     } catch (error) {
-        res.status(500).json({error:"Internal Server error"})
+        res.status(500).json({ error: "Internal Server error" })
     }
 }
 
-const create = async(req,res)=>{
+const create = async (req, res) => {
     try {
         const userData = new User(req.body);
-        const{email} = userData;
-        
-        const userExists = await User.findOne({email});
-        if(userExists)
-        {
-            return res.status(400).json({error:"User already exists with this email"});
+        const { email } = userData;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ error: "User already exists with this email" });
         }
-        
+
         const savedUser = await userData.save();
-        res.status(200).json({message:savedUser});
+        res.status(200).json({ message: savedUser });
         console.log("User saved");
     } catch (error) {
-        res.status(500).json({error:"Internal Server error"});
+        res.status(500).json({ error: "Internal Server error" });
     }
 }
 
-module.exports = {test,create};
+const read = async (req, res) => {
+    try {
+        const users = await User.find();
+        if (users.length === 0) {
+            return res.status(404).json({ error: "No users found" });
+        }
+
+        res.status(200).json({ users });
+        console.log("Users fetched");
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server error" });
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const userExists = await User.findOne({ _id: id });
+        if (!userExists) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json({ message: updatedUser });
+        console.log("User updated");
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server error" });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const userExists = await User.findOne({ _id: id });
+        if (!userExists) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        await User.findByIdAndDelete({ _id: id });
+        res.status(200).json({ message: "User deleted successfully" });
+        console.log("User deleted successfully");
+
+    } catch (error) {
+        res.ststus(500).json({ error: "Internal Server error" });
+    }
+}
+
+module.exports = { test, create, read, update, deleteUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-const {test,create} = require("../controller/userController")
+const {test,create,read,update,deleteUser} = require("../controller/userController")
 
 const router = express.Router();
 
 router.get("/test",test);
 router.post("/create",create);
+router.get("/read",read);
+router.put("/update/:id",update);
+router.delete("/delete/:id",deleteUser);
 
 module.exports = router; 
